13b6aa312ed775c3b8132990c23ad5f8186a6194
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-02 18:39:32 +0530

    Added environment instructions for IDE

---
+++ Implementation/Webapp/PythonRegexProcessor/ide.sh
@@ -0,0 +1,5 @@
+Intellij IDE
+Created Python SDK from the IDE at location ~/Projects/Environments/Python/venv3.8.11
+To apply following command:
+
+source ~/Projects/Environments/Python/venv3.8.11/bin/activate
