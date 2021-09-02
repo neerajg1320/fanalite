954974d6dc082a37b5aece4e3199195065d321ad
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-02 18:52:01 +0530

    Restructured the regex api calls in a separate file name regex_blueprint.py

--- Implementation/Webapp/PythonRegexProcessor/api.py
+++ Implementation/Webapp/PythonRegexProcessor/api.py
@@ -1,14 +1,11 @@
-from flask import Flask, jsonify, request
-from jsonschema import validate
-from jsonschema.exceptions import ValidationError
-import json
-
-from regex import check_compile_regex, regex_apply_on_text
-from exceptions import InvalidParams
-from debug_utils import debug_log
+from flask import Flask
+from regex_blueprint import rpapp
 
 
 app = Flask(__name__)
+
+app.register_blueprint(rpapp)
+
 
 DEBUG = False
 
@@ -18,97 +15,3 @@ def hello_world():
     return "PythonRegexProcessor: Flask Application"
 
 
-regex_is_valid_schema = {
-    'type': 'object',
-    'properties': {
-        'name': { 'type': 'string' },
-        'regex': { 'type': 'string' },
-        'flags': { 'type': 'object' }
-    },
-    'required': ['regex']
-}
-
-regex_apply_schema = {
-    'type': 'object',
-    'properties': {
-        'regex': { 'type': 'string' },
-        'text': {'type': 'string'},
-        'flags': { 'type': 'object' }
-    },
-    'required': ['regex', 'text']
-}
-
-
-def get_validated_json(request, schema):
-    if request.content_type is None or not 'application/json' in request.content_type:
-        raise InvalidParams("Error! json data missing")
-
-    request_data = request.get_data().decode('utf-8')
-
-    try:
-        request_dict = json.loads(request_data, strict=False)
-    except json.decoder.JSONDecodeError as e:
-        raise InvalidParams(str(e))
-
-    if request_dict is None:
-        raise InvalidParams('Error! json body is missing')
-
-    # This will raise ValidationError in case the request is different from schema
-    validate(request_dict, schema)
-
-    return request_dict
-
-
-def create_response(result=None, error=None, status=None):
-    if error is not None:
-        response = (jsonify({ "status": "failed", "error": error }),
-                    status if status is not None else 400)
-    else:
-        response = (jsonify({"status": "success", "result": result}),
-                    status if status is not None else 200)
-
-    debug_log("response:", result if error is None else error, active=False)
-    return response
-
-
-@app.errorhandler(InvalidParams)
-def handle_invalid_params(error):
-    return create_response(error=error.message)
-
-
-@app.errorhandler(ValidationError)
-def handle_validation_error(error):
-    return create_response(error=error.message)
-
-
-@app.route('/regex/validate', methods=['POST'])
-def regex_isvalid_post():
-    request_dict = get_validated_json(request, regex_is_valid_schema)
-
-    _, regex_error = check_compile_regex(request_dict["regex"])
-
-    result = {
-        'isValid': regex_error is None
-    }
-    if regex_error is not None:
-        result.update({'regex_error': regex_error})
-
-    return create_response(result=result)
-
-
-@app.route('/regex/apply', methods=['POST'])
-def regex_apply():
-    request_dict = get_validated_json(request, regex_apply_schema)
-
-    regex_str = request_dict["regex"]
-    text = request_dict["text"]
-    flags = request_dict.get("flags", None)
-
-    if DEBUG:
-        print("Regex Str:\n", regex_str)
-        print("Text:\n", text)
-        print("Flags:\n", flags)
-
-    result = regex_apply_on_text(regex_str, text, flags=flags)
-
-    return create_response(result=result, error=result["error"], status=200)
