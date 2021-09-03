16e63d5d9c25d705afbc43411c4dbc5da3fb5566
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-03 13:14:53 +0530

    Added the custom Exception Handling for missing parameters

--- Implementation/Webapp/JavaRegexProcessor/src/main/java/com/example/regexapp/regexController/RegexController.java
+++ Implementation/Webapp/JavaRegexProcessor/src/main/java/com/example/regexapp/regexController/RegexController.java
@@ -5,9 +5,13 @@ import com.example.regexapp.plain.MatchInfo;
 import com.example.regexapp.plain.RegexEngine;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -30,4 +34,16 @@ public class RegexController {
         return new ResponseEntity<>(RegexEngine.apply(body.regex, body.text), HttpStatus.OK);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
