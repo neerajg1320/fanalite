aa3234d8b59cf7f9d27fd86fc25c4c275fa8d2a0
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-06 14:57:33 +0530

    Added initial support for viewBinding in the RulesListFragment

--- Implementation/MobileApp/RulesApp/app/src/main/java/com/fanalite/rulesapp/fragments/list/RulesListFragment.kt
+++ Implementation/MobileApp/RulesApp/app/src/main/java/com/fanalite/rulesapp/fragments/list/RulesListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.fanalite.rulesapp.R
+import com.fanalite.rulesapp.databinding.FragmentRulesListBinding
 
 
 /**
@@ -14,13 +16,21 @@ import com.fanalite.rulesapp.R
  * create an instance of this fragment.
  */
 class RulesListFragment : Fragment() {
+    private var _binding: FragmentRulesListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules_list, container, false)
+        _binding = FragmentRulesListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_rulesListFragment_to_addRuleFragment)
+        }
+
+        return binding.root
     }
 
 
