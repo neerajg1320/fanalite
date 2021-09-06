bdfafb3256f4a3091d922ae8c208c047dd58b439
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-06 15:20:45 +0530

    Added support for Custom Components which are using fonts Montserrat-Regular.ttf Montserrat-Bold.ttf

---
+++ Implementation/MobileApp/RulesApp/app/src/main/java/com/fanalite/rulesapp/custom/CustomTextView.kt
@@ -0,0 +1,22 @@
+package com.fanalite.rulesapp.custom
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * This class will be used for Custom font text using the TextView which inherits the AppCompatTextView class.
+ */
+class CustomTextView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Regular.ttf")
+        setTypeface(typeface)
+
+    }
+}
---
+++ Implementation/MobileApp/RulesApp/app/src/main/java/com/fanalite/rulesapp/custom/CustomTextViewBold.kt
@@ -0,0 +1,18 @@
+package com.fanalite.rulesapp.custom
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class CustomTextViewBold(context: Context, attrs: AttributeSet ): AppCompatTextView(context, attrs) {
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val boldTypeFace: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        typeface = boldTypeFace
+    }
+}
