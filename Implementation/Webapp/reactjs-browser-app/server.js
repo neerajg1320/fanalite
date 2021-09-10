957444b04d64082afcd8b5bf8d6fe19be6095c42
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-10 13:28:34 +0530

    Added support for login fail mock

--- Implementation/Webapp/reactjs-browser-app/server.js
+++ Implementation/Webapp/reactjs-browser-app/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const cors = require('cors')
 const app = express();
+const cors = require('cors')
 
 app.use(cors());
 
+
 app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
+    res.send({
+      token: 'test123'
+    });
 });
+
+app.use('/login/bad', (req, res) => {
+  res.send({}, 401);
+});
+
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
