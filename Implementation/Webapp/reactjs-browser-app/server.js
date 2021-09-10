1ec6d822a4f4a02f72fa61df1a1100f47a753b70
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-10 13:49:48 +0530

    Added code for loggin the API calls

--- Implementation/Webapp/reactjs-browser-app/server.js
+++ Implementation/Webapp/reactjs-browser-app/server.js
@@ -4,14 +4,47 @@ const cors = require('cors')
 
 app.use(cors());
 
+let demoLogger = (req, res, next) => {
+  console.log("Hello from logger");
+  next();
+};
+
+// Ref: https://codesource.io/creating-a-logging-middleware-in-expressjs/
+
+let apiLogger = (req, res, next) => {
+  let current_datetime = new Date();
+  let formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  let log = `[${formatted_date}] ${method}:${url} ${status}`;
+  console.log(log);
+  next();
+};
+
+// app.use(demoLogger);
+app.use(apiLogger);
 
 app.use('/login', (req, res) => {
+    console.log("req.body", req.body);
+
     res.send({
       token: 'test123'
     });
 });
 
-app.use('/login/bad', (req, res) => {
+app.use('/badlogin', (req, res) => {
   res.send({}, 401);
 });
 
