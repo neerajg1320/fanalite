5c7e268724039006b4c3c7fcc98d40701278fca0
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-20 10:58:29 +0530

    Added description to the instructions in run.cmd

--- Implementation/Servers/DistributedScaledBackend/FlinkKafkaStreamProcessor/run.cmd
+++ Implementation/Servers/DistributedScaledBackend/FlinkKafkaStreamProcessor/run.cmd
@@ -2,25 +2,34 @@
 
 # Setup Flink
 ~/Projects/Flink/flink-1.5.0/bin/start-cluster.sh
-cd ~/Projects/Kafka/kafka_2.13-2.8.0
 
 # Setup Kafka
 # We do not need to start Zookeper as it is started by the Flink start-cluster
 # bin/zookeeper-server-start.sh config/zookeeper.properties
+cd ~/Projects/Kafka/kafka_2.13-2.8.0
 bin/kafka-server-start.sh config/server.properties
 
 
 ## Start Application
 
 # Terminal 1:
+# Produce messages on topic 'text'
 ~/Projects/Kafka/kafka_2.13-2.8.0/bin/kafka-console-producer.sh --topic text  --bootstrap-server localhost:9092
 
 # Terminal 2:
-~/Projects/Kafka/kafka_2.13-2.8.0/bin/kafka-console-consumer.sh --topic transactions  --from-beginning --bootstrap-server localhost:9092
+# Subscribe to messages on topic 'text'
+~/Projects/Kafka/kafka_2.13-2.8.0/bin/kafka-console-consumer.sh --topic text  --from-beginning --bootstrap-server localhost:9092
 
 # Terminal 3:
+# Start Flink application. 
+# The application listen to messages on topic 'text' and produced the results on topic 'transactions'
 cd /home/neeraj/mac_project/fanalite/Implementation/Servers/DistributedScaledBackend/FlinkKafkaStreamProcessor
 ~/Projects/Flink/flink-1.5.0/bin/flink run ./target/FlinkKafkaStreamProcessor-0.1-jar-with-dependencies.jar --kafka-host localhost:9092  --input-topic text --output-topic transactions
+
+
+# Terminal 2:
+# Subscribe to messages on topic 'transactions'
+~/Projects/Kafka/kafka_2.13-2.8.0/bin/kafka-console-consumer.sh --topic transactions  --from-beginning --bootstrap-server localhost:9092
 
 
 ## Sample Input
