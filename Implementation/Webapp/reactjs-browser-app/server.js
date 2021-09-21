2ca79033c0f265a502f24bc9d6d259d0a34fbd54
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-21 11:39:21 +0530

    Added message to stub server

--- Implementation/Webapp/reactjs-browser-app/server.js
+++ Implementation/Webapp/reactjs-browser-app/server.js
@@ -56,4 +56,7 @@ app.use('/login', (req, res) => {
 
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(8080, () => {
+  console.log('API is running on http://localhost:8080/login');
+  console.log('Use username "alice" for a successful login');
+});
