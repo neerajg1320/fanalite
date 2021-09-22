5e8f1225ea7cb58f194f631366ae1dff7e03a7da
Neeraj Gupta <neeraj76@yahoo.com>
2021-09-22 12:07:48 +0530

    added api debug for showing headers as well

--- Implementation/Servers/DistributedScaledBackend/RealtimeRestServer/src/middleware/index.js
+++ Implementation/Servers/DistributedScaledBackend/RealtimeRestServer/src/middleware/index.js
@@ -26,9 +26,10 @@ let apiLogger = (req, res, next) => {
     current_datetime.getMinutes() +
     ":" +
     current_datetime.getSeconds();
-  let method = req.method;
-  let url = req.url;
-  let log = `[${formatted_date}] ${method}:${url}`;
+
+  let log = `[${formatted_date}] ${req.method}:${req.url} body:${JSON.stringify(req.body)} headers: ${JSON.stringify(req.headers)}`;
+
+
   console.log(log);
   next();
   let status = res.statusCode;
